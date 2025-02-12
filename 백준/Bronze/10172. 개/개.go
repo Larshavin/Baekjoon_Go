2452b558528b0232ae3774c830042a0f1492900e
양성연 <89558536+Larshavin@users.noreply.github.com>
2025-02-12 22:17:21 +0900

    [Bronze V] Title: 개, Time: 4 ms, Memory: 816 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.go
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/10172
+// Q: 아래 예제와 같이 개를 출력하시오.
+// Output: 개를 출력한다.
+
+// ex)
+// |\_/|
+// |q p|   /}
+// ( 0 )"""\
+// |"^"`    |
+// ||_/=\\__|
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"` + "`" + `    |
+||_/=\\__|`)
+
+}
