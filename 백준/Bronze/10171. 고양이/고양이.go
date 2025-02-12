bdd8d2e002dd975dcf3c22da1d3f30900f8ed9db
양성연 <89558536+Larshavin@users.noreply.github.com>
2025-02-12 22:12:59 +0900

    [Bronze V] Title: 고양이, Time: 4 ms, Memory: 844 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.go
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/10171
+
+// Q: 아래 예제와 같이 고양이를 출력하시오.
+// Output: 고양이를 출력한다.
+
+// ex )
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(`\    /\
+ )  ( ')
+(  /  )
+ \(__)|`)
+
+}
