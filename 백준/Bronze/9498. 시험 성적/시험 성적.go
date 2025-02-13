b2dacb016f2cebdd8b5f441efc215fe15d9ac3d4
양성연 <89558536+Larshavin@users.noreply.github.com>
2025-02-13 23:07:30 +0900

    [Bronze V] Title: 시험 성적, Time: 4 ms, Memory: 872 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.go
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/9498
+/*
+Q) 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+input: 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+output: 시험 성적을 출력한다.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// Your solution here
+	var score int
+	fmt.Scan(&score)
+
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score >= 80 {
+		fmt.Println("B")
+	} else if score >= 70 {
+		fmt.Println("C")
+	} else if score >= 60 {
+		fmt.Println("D")
+	} else {
+		fmt.Println("F")
+	}
+}
