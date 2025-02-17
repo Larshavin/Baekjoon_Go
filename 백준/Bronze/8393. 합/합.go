0d56029939dfab520171dd8ed7d8c3eafa39acb6
양성연 <89558536+Larshavin@users.noreply.github.com>
2025-02-17 23:33:30 +0900

    [Bronze V] Title: 합, Time: 4 ms, Memory: 852 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.go
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/8393
+/*
+Q) n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+input: 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+output: 1부터 n까지 합을 출력한다.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Your solution here
+	var n int
+	fmt.Scan(&n)
+
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
